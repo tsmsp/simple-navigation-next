19d3d96fff929579a58ec34f082d2da072c461d8
tsmsp <tsm_sp@hotmail.com>
2023-06-07 12:21:29 -0700

    general style

---
+++ src/theme/GlogalStyle.js
@@ -0,0 +1,10 @@
+export default function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      body {
+        color: blue;
+        font-family: sans-serif;
+      }
+    `}</style>
+  );
+}
