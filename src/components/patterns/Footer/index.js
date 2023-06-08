28835c7ee125483aafe190736242cb0200ddeef0
tsmsp <tsm_sp@hotmail.com>
2023-06-07 22:27:47 -0700

    Footer component

---
+++ src/components/patterns/Footer/index.js
@@ -0,0 +1,43 @@
+import { theme } from "../../../theme/theme";
+import { Box, Text } from "../../../theme/components";
+
+export default function Footer() {
+  return (
+    <Box
+      as="footer"
+      // @ts-ignore
+      styleSheet={{
+        backgroundColor: theme.colors.primary[900],
+      }}
+    >
+      <Box
+        // @ts-ignore
+        styleSheet={{
+          overflow: "hidden",
+          maxWidth: theme.space.xcontainer_xl,
+          marginLeft: "auto",
+          marginRight: "auto",
+          paddingVertical: {
+            xs: theme.space.x12,
+          },
+          paddingHorizontal: {
+            xs: theme.space.x4,
+            sm: theme.space.x6,
+            lg: theme.space.x8,
+          },
+        }}
+      >
+        <Text
+          as="p"
+          styleSheet={{
+            textVariant: theme.typography.variants.body3,
+            textAlign: "center",
+            color: theme.colors.neutral[400],
+          }}
+        >
+          &copy; {new Date().getFullYear()} Super Pepper. All rights reserved.
+        </Text>
+      </Box>
+    </Box>
+  );
+}
